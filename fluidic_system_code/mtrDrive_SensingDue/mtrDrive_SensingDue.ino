0ebd74f128af02ba7fb78624186388536f9aa6ab
claytonlittle <clayton.little@live.com>
2016-08-11 12:01:03 -0500

    manual sync

--- fluidic_system_code/mtrDrive_SensingDue/mtrDrive_SensingDue.ino
+++ fluidic_system_code/mtrDrive_SensingDue/mtrDrive_SensingDue.ino
@@ -5,10 +5,18 @@
    Connects second and third syringe with 3-way valve.
 
    This codes expects a message in the format: 1,4567,890*
-   The first field is an indicator of 1 or 0 for direction
-   The rest of the fields can be arbitrary size
+   <motor>,<direction>,<microLiters>,<microLiters/sec><*>
+   The fields can be of arbitrary length. They must be integers.
    This code requires some arbitrary character
    to indicate the end of the command. Here I have used "*"
+
+   ************************************************************
+   "Motor 4" actually represents the case where motor 1 and 2 are
+   pumping in opposite directions, to produce a fluid flow. I didn't
+   have time to implement a library like AccelStepper, which allows for
+   simultaneous motor driving. This solution is temporary and should be
+   replaced.
+   ************************************************************
 */
 //#include <Scheduler.h>
 
@@ -95,18 +103,23 @@ void setup() {
   pinMode(valvepin, OUTPUT);
 
   //Set up interrupts:
-  attachInterrupt(digitalPinToInterrupt(LSin),checkLS,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(LSin), checkLS, CHANGE);
 
   //Prepare for motor driving:
-  resetPins();     //Set all pins to default states
+  resetPins();        //Set all pins to default states
   Serial.begin(9600); //Open serial connection for displaying/debugging
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
 
   //Print control instructions:
-  Serial.println("Begin fluid control\n");
-  Serial.println("Enter parameters");
-  Serial.println("Usage: <motor><direction>,<microLiters>,<microLiters/sec><*>");
-  Serial.println("Example: 1,1,1000,100*");
-  Serial.println("Where direction is given by 1 or 0 for in or out, respectively.\n");
+  Serial.print(
+    "Begin fluid control\n\n"
+    "Enter parameters\n"
+    "Usage: <motor>,<direction>,<microLiters>,<microLiters/sec><*>\n"
+    "Example: 1,1,1000,100*\n"
+    "Where direction is given by 1 or 0 for in or out, respectively.\n\n");
+  delay(1000);
 }
 
 
@@ -114,7 +127,7 @@ void setup() {
 
 //Main loop
 void loop() {
-  //Loop to acquire the serial fields:
+  //Loop to acquire the serial fields
   while (done == 0) {
     if (Serial.available()) {
       ui = Serial.read();
@@ -133,28 +146,34 @@ void loop() {
 
   //Diagnostic print statements
   /*
-  Serial.print("Motor: "); Serial.println(values[0]);
-  Serial.print("Direction: "); Serial.println(values[1]);
-  Serial.print("Volume: "); Serial.println(values[2]);
-  Serial.print("Speed: "); Serial.println(values[3]);
-  Serial.println();
+    Serial.print("Motor: "); Serial.println(values[0]);
+    Serial.print("Direction: "); Serial.println(values[1]);
+    Serial.print("Volume: "); Serial.println(values[2]);
+    Serial.print("Speed: "); Serial.println(values[3]);
+    Serial.println();
   */
 
-  //Carry out command:
-  for (int i = 0; i < 6; i++) {
-    dpins[i] = pins[values[0] - 1][i];
+  //Find motor pins
+  if (values[0] != 4) { //4 is not a real motor
+    for (int i = 0; i < 6; i++) {
+      dpins[i] = pins[values[0] - 1][i];
+    }
   }
+
+  //Send command
   if (values[0] == 3)
     PrecisionSyringe();
+  else if (values[0] == 4)
+    BasicSyringeFlow();
   else
     BasicSyringe();
 
-  //Reset for next command:
+  //Reset for next command
   for (int i = 0; i <= fieldIndex; i++)
     values[i] = 0;
   fieldIndex = 0;
   resetPins();
-  delay(10);
+  delay(100);
 }
 
 
@@ -185,7 +204,7 @@ void BasicSyringe()
   digitalWrite(dpins[0], LOW);
   //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
   digitalWrite(dpins[1], HIGH); digitalWrite(dpins[2], HIGH); digitalWrite(dpins[3], HIGH);
-  
+
   //DIRECTION
   if (values[1] == 1)
     digitalWrite(dpins[5], HIGH); //Pull direction pin high to inject
@@ -197,7 +216,7 @@ void BasicSyringe()
   //SPEED
   spd_t = ceil(mcs_stp / values[3] / 2);
   Serial.print("Delay: "); Serial.println(spd_t); Serial.println();
-  
+
   //DRIVE
   if (spd_t <= 16383) //microsecond delay is only accurate for such values
   {
@@ -231,12 +250,12 @@ void PrecisionSyringe()
   //Enable limit switch and open 3-way valve
   digitalWrite(LSout, HIGH);
   digitalWrite(valvepin, HIGH); //valve is normally closed, now we open
-  
+
   //Pull EN down to enable FETs and allow motor to move
   digitalWrite(dpins[0], LOW);
   //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
   digitalWrite(dpins[1], HIGH); digitalWrite(dpins[2], HIGH); digitalWrite(dpins[3], HIGH);
-  
+
   //DIRECTION
   if (values[1] == 1)
     digitalWrite(dpins[5], HIGH); //Pull direction pin high to inject
@@ -248,7 +267,7 @@ void PrecisionSyringe()
   //SPEED
   spd_t = ceil(mcs_stp_PS / values[3] / 2);
   Serial.print("Delay: "); Serial.println(spd_t);
-  
+
   //DRIVE
   if (spd_t <= 16383) //microsecond delay is only accurate for such values
   {
@@ -284,9 +303,64 @@ void PrecisionSyringe()
 
 
 
-void checkLS() 
+
+// Drive blank and waste syringes at same time
+void BasicSyringeFlow()
 {
-  stopmtr = digitalRead(LSin);
-  stopmtr = digitalRead(LSin);
+  //Pull EN down to enable FETs and allow motor to move
+  digitalWrite(EN_1, LOW); digitalWrite(EN_2, LOW);
+  //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
+  digitalWrite(MS1, HIGH); digitalWrite(MS2, HIGH); digitalWrite(MS3, HIGH);
+
+  //DIRECTION
+  if (values[1] == 1) {
+    digitalWrite(dir_1, HIGH); //Pull direction pin high to inject
+    digitalWrite(dir_2, LOW);  //Pull direction pin low to remove
+  }
+  else {
+    digitalWrite(dir_1, LOW); //Pull direction pin high to inject
+    digitalWrite(dir_2, HIGH);  //Pull direction pin low to remove
+  }
+  //VOLUME
+  xlim = ceil(values[2] * stp_mcl * errorCorrection);
+  Serial.print("Steps: "); Serial.println(xlim);
+  //SPEED
+  spd_t = ceil(mcs_stp / values[3] / 2);
+  Serial.print("Delay: "); Serial.println(spd_t); Serial.println();
+
+  //DRIVE
+  if (spd_t <= 16383) //microsecond delay is only accurate for such values
+  {
+    for (int x = 1; x <= xlim; x++) //Loop the forward stepping enough times for motion to be visible
+    {
+      digitalWrite(stp_1, HIGH); //Trigger one step forward
+      digitalWrite(stp_2, HIGH); //Trigger one step forward
+      delayMicroseconds(spd_t);
+      digitalWrite(stp_1, LOW);  //Pull step pin low so it can be triggered again
+      digitalWrite(stp_2, LOW);  //Pull step pin low so it can be triggered again
+      delayMicroseconds(spd_t);
+    }
+  }
+  else //must use millisecond delay
+  {
+    spd_t = ceil(spd_t / 1000.0);  //convert to milliseconds
+    for (int x = 1; x <= xlim; x++) //Loop the forward stepping enough times for motion to be visible
+    {
+      digitalWrite(stp_1, HIGH); //Trigger one step forward
+      digitalWrite(stp_2, HIGH); //Trigger one step forward
+      delay(spd_t);
+      digitalWrite(stp_1, LOW);  //Pull step pin low so it can be triggered again
+      digitalWrite(stp_2, LOW);  //Pull step pin low so it can be triggered again
+      delay(spd_t);
+    }
+  }
 }
 
+
+
+//ISR to stop the Precision Syringe after limit switch
+void checkLS()
+{
+  stopmtr = digitalRead(LSin);
+  stopmtr = digitalRead(LSin); //must read twice. Reading once is unreliable.
+}
